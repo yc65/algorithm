0c51c75d6540ac0dcfa1ebeea988141123b1f8e6
Yc65 <xuyichen65@outlook.com>
2018-12-23 11:54:06 +0800

    week2 lecture count inversions

---
+++ counting_inversion/counting_inversion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  counting_inversion
+//
+//  Created by Yichen_Xu on 2018/12/22.
+//  Copyright © 2018 yc65. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
